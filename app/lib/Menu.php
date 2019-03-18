ad46241c1decbd9199f505f411f7e7fc92ba6691
吳姿萱 <cherry51120@gmail.com>
2019-03-18 21:10:23 +0800

    fix

--- app/lib/Menu.php
+++ app/lib/Menu.php
@@ -6,7 +6,7 @@ use \OA\Line\Message as Msg;
 
 class Menu {
   public static function orderInfo() {
-    Log::error(123);
+    Log::info(123);
     return (
       Msg::flex()->altText('訂單資料')->template(
         Msg\FlexTemplate::bubble([
