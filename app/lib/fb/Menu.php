16595fb189acb6bee92f90d897af8425b54439cd
吳姿萱 <cherry51120@gmail.com>
2019-06-04 22:05:27 +0800

    order search

--- app/lib/fb/Menu.php
+++ app/lib/fb/Menu.php
@@ -24,7 +24,7 @@ class Menu {
   public static function quickOrder($source) {
     return new QuickReply($source->sid, '訂單查詢', 
       [
-        new QuickReplyButton(QuickReplyButton::TYPE_TEXT, '訂單查詢', json_encode(['order'])),
+        new QuickReplyButton(QuickReplyButton::TYPE_TEXT, '點我「 查詢訂單 」', json_encode(['order'])),
       ]
     );
   }
