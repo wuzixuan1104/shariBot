b30774cce53678b25c28a4c32fd8024f254053ee
wu-tzu-hsuan <cherry51120@gmail.com>
2019-06-04 10:25:37 +0800

    menu

--- app/migration/021-create_FbSource.php
+++ app/migration/021-create_FbSource.php
@@ -4,7 +4,7 @@ return [
   'up' => "CREATE TABLE `FbSource` (
     `id`            int(11) unsigned NOT NULL AUTO_INCREMENT,
     `sid`           varchar(50)  COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '來源 ID',
-    `menuVersion`   varchar(50) unsigned NOT NULL DEFAULT 0 COMMENT '常用菜單版號',
+    `menuVersion`   varchar(50)  COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '常用菜單版號',
     `title`         varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '標題',
     `token`         varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'Token',
     `updateAt`      datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新時間',
